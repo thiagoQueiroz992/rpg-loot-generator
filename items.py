b3700fc73ebe0c6ec02d9f4d9d0b99c655fb14a6
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-02-21 11:07:46 -0300

    individual item classes declared

--- items.py
+++ items.py
@@ -3,3 +3,62 @@ class Item:
         self.item_id = "item:null"
         self.item_name = "Null"
 
+class String(Item):
+    def __init__(self):
+        super().__init__()
+        self.item_id = "item:string"
+        self.item_name = "String"
+
+class RottenFlesh(Item):
+    def __init__(self):
+        super().__init__()
+        self.item_id = "item:rotten_flesh"
+        self.item_name = "Rotten Flesh"
+
+class Gunpowder(Item):
+    def __init__(self):
+        super().__init__()
+        self.item_id = "item:gunpowder"
+        self.item_name = "Gunpowder"
+
+class Arrow(Item):
+    def __init__(self):
+        super().__init__()
+        self.item_id = "item:arrow"
+        self.item_name = "Arrow"
+
+class SpiderEye(Item):
+    def __init__(self):
+        super().__init__()
+        self.item_id = "item:spider_eye"
+        self.item_name = "Spider Eye"
+
+class IronIngot(Item):
+    def __init__(self):
+        super().__init__()
+        self.item_id = "item:iron_ingot"
+        self.item_name = "Iron Ingot"
+
+class Coal(Item):
+    def __init__(self):
+        super().__init__()
+        self.item_id = "item:coal"
+        self.item_name = "Coal"
+
+class Stick(Item):
+    def __init__(self):
+        super().__init__()
+        self.item_id = "item:stick"
+        self.item_name = "Stick"
+
+class Feather(Item):
+    def __init__(self):
+        super().__init__()
+        self.item_id = "item:feather"
+        self.item_name = "Feather"
+
+class Wool(Item):
+    def __init__(self):
+        super().__init__()
+        self.item_id = "item:wool"
+        self.item_name = "Wool"
