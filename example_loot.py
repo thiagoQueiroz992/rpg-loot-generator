cc940b5b41eddefe6d55f06282bfb1c123e7d824
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-02-22 09:40:15 -0300

    example loot table items added

--- example_loot.py
+++ example_loot.py
@@ -1,5 +1,14 @@
 import items
 
 example_loot_table = [
-    
+    [items.String, 75, 1, 8],
+    [items.RottenFlesh, 100, 3, 12],
+    [items.Gunpowder, 65, 1, 6],
+    [items.Arrow, 86, 3, 7],
+    [items.SpiderEye, 27, 1, 3],
+    [items.IronIngot, 38, 1, 5],
+    [items.Coal, 58, 2, 5],
+    [items.Stick, 100, 2, 10],
+    [items.Feather, 62, 1, 7],
+    [items.Wool, 70, 2, 8]
 ]
