a8528d6eb64f4e060de5beb1fa8fb28a04406158
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-02-22 09:52:06 -0300

    loot generator function update 2

--- functions.py
+++ functions.py
@@ -6,4 +6,4 @@ def loot_generator(loot_table: list):
         if random.random() <= i[1] * 0.01:
             item_quant = random.randint(i[2], i[3])
             loot_items.append([i[0], item_quant])
-            print(loot_items[-1])
+            print(loot_items[-1][0]().item_name, "x", loot_items[-1][1])
