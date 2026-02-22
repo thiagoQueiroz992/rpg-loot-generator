1d713c56930704a613da087c74bda2daaaa5bac7
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-02-22 16:58:50 -0300

    colors and final changes

--- functions.py
+++ functions.py
@@ -2,17 +2,17 @@ import random, time
 
 def loot_generator(loot_table: list):
     loot_items = list()
-    print("Generating loot", end="", flush=True)
+    print("\n\033[33mGenerating loot\033[m", end="", flush=True)
     time.sleep(1)
     for d in range(1, 4):
         print(".", end="", flush=True)
         time.sleep(1)
     print()
-    print("Loot has been generated!")
+    print("\033[32mLoot has been generated!\033[m\n")
     time.sleep(.5)
     for i in loot_table:
         if random.random() <= i[1] * 0.01:
             item_quant = random.randint(i[2], i[3])
             loot_items.append([i[0], item_quant])
-            print(loot_items[-1][0]().item_name, "x", loot_items[-1][1])
+            print("\033[1;34m" + loot_items[-1][0]().item_name + "\033[m", "x", "\033[3m" + str(loot_items[-1][1]) + "\033[m")
             time.sleep(1)
