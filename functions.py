08c7b52d54c1de39848a62d2e734433476711c01
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-02-22 09:30:02 -0300

    loot generator function generated_max_item_quant parameter removed

--- functions.py
+++ functions.py
@@ -1,10 +1,9 @@
 import random
 
-def loot_generator(loot_table: list, generated_item_max_quant: int):
+def loot_generator(loot_table: list):
     loot_items = list()
     for i in loot_table:
-        if i[1] * 0.01 <= random.random():
+        if random.random() <= i[1] * 0.01:
             item_quant = random.randint(i[2], i[3])
             loot_items.append([i[0], item_quant])
             print(loot_items[-1])
-            if generated_item_max_quant >= len(loot_items): break
