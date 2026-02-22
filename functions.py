bf52eb0d96039a28992ee947ae57dbf8d2eee7dd
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-02-22 09:19:43 -0300

    loot generator function logic not tested

--- functions.py
+++ functions.py
@@ -1,4 +1,10 @@
 import random
 
 def loot_generator(loot_table: list, generated_item_max_quant: int):
-    pass
+    loot_items = list()
+    for i in loot_table:
+        if i[1] * 0.01 <= random.random():
+            item_quant = random.randint(i[2], i[3])
+            loot_items.append([i[0], item_quant])
+            print(loot_items[-1])
+            if generated_item_max_quant >= len(loot_items): break
