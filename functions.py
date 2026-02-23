ecdc8cdc4b62ed1cadcfecd38c5dd4c6d482932b
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-02-23 10:15:39 -0300

    loot showing as shuffle

--- functions.py
+++ functions.py
@@ -8,11 +8,15 @@ def loot_generator(loot_table: list):
         print(".", end="", flush=True)
         time.sleep(1)
     print()
-    print("\033[32mLoot has been generated!\033[m\n")
-    time.sleep(.5)
+
     for i in loot_table:
         if random.random() <= i[1] * 0.01:
             item_quant = random.randint(i[2], i[3])
             loot_items.append([i[0], item_quant])
-            print("\033[1;34m" + loot_items[-1][0]().item_name + "\033[m", "x", "\033[3m" + str(loot_items[-1][1]) + "\033[m")
-            time.sleep(1)
+    print("\033[32mLoot has been generated!\033[m\n")
+    random.shuffle(loot_items)
+    time.sleep(.5)
+    
+    for i in loot_items:
+        print("\033[1;34m" + i[0]().item_name + "\033[m", "x", "\033[3m" + str(i[1]) + "\033[m")
+        time.sleep(1)
