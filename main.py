774ed3cc364634351637bf69ce8c078065055fc6
Thiago Queiroz <thsqueiroz.2k26@gmail.com>
2026-04-08 08:54:53 -0300

    removed visual parts

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import functions, example_loot
 
 input("Click Enter to generate loot")
 
-functions.loot_generator(example_loot.example_loot_table)
-print()
+ex = functions.loot_generator(example_loot.example_loot_table)
+print(ex)
